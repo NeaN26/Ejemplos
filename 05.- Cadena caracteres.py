5423f75673a833cde4ef87a11626d1c9bda61e58
Nean26 <nelson.pachecod@gmail.com>
2020-04-14 18:49:56 -0400

    ejercicio tupla

---
+++ 05.- Cadena caracteres.py
@@ -0,0 +1,33 @@
+lenguajes = "Python; Java; Ruby; Swift; JavaScript; C#; C; C++"
+
+#Separar cadena por espacio
+
+resultado = lenguajes.split()
+print(resultado)
+
+# separar por ;
+resultado2 = lenguajes.split(";")
+print(resultado2)
+
+# sin ; ni espacios
+separador = "; "
+resultado3 = lenguajes.split(separador)
+print(resultado3)
+
+# Unir con espacios
+nuevo_string = " ".join(resultado3)
+print(nuevo_string)
+
+# Unir con ;
+nuevo_string2 = separador.join(resultado3)
+print(nuevo_string2)
+
+texto = """ Hola! Hola!
+Cómo estás?
+chao
+chao
+
+felicidad.
+"""
+resultado4 = texto.splitlines()
+print(resultado4)
